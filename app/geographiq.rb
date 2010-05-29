5a4e78ec1ae245dfc824d5cffea13d3d3e218002
maetl <me@maetl.net>
2010-05-29 22:26:12 +0200

    moving errant comment

--- app/geographiq.rb
+++ app/geographiq.rb
@@ -12,7 +12,6 @@ module Geographiq
     use(Rack::Conneg) do |conneg|
       conneg.set :accept_all_extensions, false
       conneg.set :fallback, :html
-      #conneg.ignore_contents_of(File.join(File.dirname(__FILE__),'docs'))
       conneg.provide([:txt, :json])
     end
     
